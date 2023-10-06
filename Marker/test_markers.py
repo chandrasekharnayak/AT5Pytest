de3f9a8da45b3b8893a270c95d9a4978bdc8be3a
Chandra Sekhar <k.csnayak@gmail.com>
2023-10-06 20:46:42 +0530

    pytest-3

--- Marker/test_markers.py
+++ Marker/test_markers.py
@@ -84,15 +84,41 @@ import pytest
 #     assert result == expected
 
 
-import pytest
-import time
+# import pytest
+# import time
+#
+# def perform_compex_calculation():
+#     time.sleep(3)
+#     return 'Result'
+#
+# @pytest.mark.timeout(2)
+# def test_complex_calculation_timeout():
+#     result = perform_compex_calculation()
+#     assert result =='Result'
 
-def perform_compex_calculation():
-    time.sleep(3)
-    return 'Result'
+#Custome Markers
 
-@pytest.mark.timeout(2)
-def test_complex_calculation_timeout():
-    result = perform_compex_calculation()
-    assert result =='Result'
+# import pytest
+#
+# @pytest.mark.smoke
+# def test_1():
+#     assert True
+#
+# @pytest.mark.regression
+# def test_2():
+#     assert True
+#
+# @pytest.mark.regression
+# def test_3():
+#     assert True
+#
+# @pytest.mark.smoke
+# def test_4():
+#     assert True
+#
+# def test_5():
+#     assert True
+#
+# def test_6():
+#     assert True
 
